df920d6bdb5bcb9f3a58d3903e9aa5f9cfbf0a4f
Thane <thanestorley@exosite.com>
2017-04-24 13:47:03 -0500

    Minor formatting changes

--- Exosite.h
+++ Exosite.h
@@ -2,20 +2,20 @@
 //
 // exosite.h - Prototypes for the Exosite Cloud API
 //
-// Copyright (c) 2012 Exosite LLC.  All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without 
+// Copyright (c) 2012-2017 Exosite LLC.  All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 
 //  * Redistributions of source code must retain the above copyright notice,
 //    this list of conditions and the following disclaimer.
-//  * Redistributions in binary form must reproduce the above copyright 
+//  * Redistributions in binary form must reproduce the above copyright
 //    notice, this list of conditions and the following disclaimer in the
 //    documentation and/or other materials provided with the distribution.
 //  * Neither the name of Exosite LLC nor the names of its contributors may
-//    be used to endorse or promote products derived from this software 
+//    be used to endorse or promote products derived from this software
 //    without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
 // NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
 // NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
